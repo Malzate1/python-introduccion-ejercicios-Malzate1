d6cc9728a32657a9549b97b8373de7933f5c569d
Mayra Alzate <nadja.minni@gmail.com>
2026-02-15 17:11:39 -0500

    Reubicacion linea ej2

--- ejercicios/ejercicio_02.py
+++ ejercicios/ejercicio_02.py
@@ -6,6 +6,7 @@
 
 # En este punto debe empezar a usar la interpolación para concatenar las variables a imprimir en consola.Recuerde que debe ingresar las variables en llaves dentro del parentesis del print().
 
+#Tip: La estructura a emplear para imprimir es print(f"Su nombre es {la variable} y su edad es {la variable}").
+
 # Ejecutar el programa.
 
-#Tip: La estructura a emplear para imprimir es print(f"Su nombre es {la variable} y su edad es {la variable}").
