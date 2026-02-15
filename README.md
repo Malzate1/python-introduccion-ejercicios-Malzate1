0f57360a5bfc5b97b1455265e788e1495efdd0e0
Mayra Alzate <nadja.minni@gmail.com>
2026-02-15 17:08:16 -0500

    Ejercicio 2

--- README.md
+++ README.md
@@ -8,5 +8,6 @@ Por Mayra Alejandra Alzate Sánchez.
 
 La temática a emplear para la creación de los ejercicios esta basada en los temas vistos en la Semana 2.
 
-1. Uso de la función print() para Python
+1. Ejercicio 1: Uso de la función print() para Python.
+2. Ejercicio 2: Implementación de interpolación para cadenas de texto.
 
---
+++ ejercicios/ejercicio_02.py
@@ -0,0 +1,11 @@
+#Ejercicio 2: Implementación de interpolación para cadenas de texto.
+
+# Esta herramienta nos permite "concatenar una cadena de palabras o strings/int" sin la necesidad de utilizar el operador +.
+
+# Defina varias variables. Ej: nombre = "" , edad = ...
+
+# En este punto debe empezar a usar la interpolación para concatenar las variables a imprimir en consola.Recuerde que debe ingresar las variables en llaves dentro del parentesis del print().
+
+# Ejecutar el programa.
+
+#Tip: La estructura a emplear para imprimir es print(f"Su nombre es {la variable} y su edad es {la variable}").
