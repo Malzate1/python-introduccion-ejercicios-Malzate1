dc8192aafea11716f6847313fac844cd7ab4ab78
Mayra Alzate <nadja.minni@gmail.com>
2026-02-23 15:52:38 -0500

    correción 8

--- ejercicios/ejercicio_08.py
+++ ejercicios/ejercicio_08.py
@@ -2,9 +2,18 @@
 
 # Descripción : Con la ayuda de condicionales, aplicaremos el uso del operador módulo e identificar si un número es par o impar. Debemos implementar el ingreso de datos del usuario para aplicar el funcionamiento del script construido.
 
-# 1. Define la variable de captura. Recuerda implementar el "input" para iniciar el uso de la consola.
+# 1. Define la variable de captura de los números a ingresar.Recuerda implementar el "input" para iniciar el uso de la consola.
+
+num=int(input("Introduce un número entero: "))
 
 # 2. Utiliza la estructura de condicionales y el operador módulo para definir el ciclo que evaluará el número que introduzca el usuario y finalmente se pueda realizar la verificación del mismo.
+
+
+if num % 2 == 0:
+    print(f"El {num} es un número par")
+
+else :
+    print(f"El {num} es un número impar")
 
 # 3. Ejecuta el programa.
 
@@ -12,11 +21,4 @@
 #       No olvides convertir la variable de entrada para que no haya errores al ejecutar el script.
 
 
-num=int(input("Introduce un número entero: "))
-
-if num % 2 == 0:
-    print(f"El {num} es un número par")
-
-else :
-    print(f"El {num} es un número impar")
 
