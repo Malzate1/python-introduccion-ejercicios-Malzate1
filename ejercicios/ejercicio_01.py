dd6061741f18dfc7ce693e21f75cf693ece6d7ad
Mayra Alzate <nadja.minni@gmail.com>
2026-02-23 17:19:31 -0500

    nuevo 1

--- ejercicios/ejercicio_01.py
+++ ejercicios/ejercicio_01.py
@@ -7,11 +7,19 @@
 # Define una variable en formato snake_case(como desees nombrarla).
 
 frase_de_vida = "Mente estoica, corazón romántico"
+numeros = [7,14,21,24,27,30]
+num= 7.9
+nombre = "Emily"
+edad = 25
+
 
 
 # Utiliza la funcion print para imprimir la variable que acabas de definir y a continuación ejecuta el programa. 
 
 print(frase_de_vida)
+print(*numeros)
+print(f"El promedio es {num:.2f}")
+print("Hola {}, tienes {} años".format(nombre, edad))
 
 
 
