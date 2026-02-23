b75ae9b0a7abdb1e73ef8fd316393472d627e410
Mayra Alzate <nadja.minni@gmail.com>
2026-02-23 16:29:19 -0500

    mejora codigo 10

--- ejercicios/ejercicio_10.py
+++ ejercicios/ejercicio_10.py
@@ -2,13 +2,13 @@
 
 # Descripción: Con la ayuda de la función range, generaremos números que posteriormente se convertiran en una lista.La función tiene un inicio, un fin y un salto que es definido en los argumentos de la misma. 
 
-# 1. Definimos la función range() con los 3 parámetros que configuran el inicio, el fin y el salto que queremos que se ejecute. Ej: inicio 2, final 20 con un salto de 3. Así : range(2,20,3).
+# 1. Define el rango y asígnale una variable.
 
-range(5,20,4)
+rango_def = range(5,20,4)
 
-# 2.Para convertir el resultado de la función en una lista, imprimimos la lista conteniendo el rango.
+# 2.Convertimos el rango a una lista e imprimimos.
 
-print(list(range(5,20,4)))
+print(list(range(range)))
 
 # 3. Ejecutamos el script.
 
