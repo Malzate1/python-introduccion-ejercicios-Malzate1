a33200174cc90c57d24d905755a303d3783b115d
Mayra Alzate <nadja.minni@gmail.com>
2026-02-23 16:23:48 -0500

    correcion 3

--- ejercicios/ejercicio_03.py
+++ ejercicios/ejercicio_03.py
@@ -6,9 +6,16 @@
 
 # 1. Define un set de numeros enteros.
 
+set_impar = {3,5,7,17,19}
+
 # 2. Define la variable que recibirá el total para aplicar la suma. A continuación emplea la palabra reservada para suma y entre parentesis, escribe la variable que  utilizaste para el set.
 
+total_set= sum(set_impar)
+
+
 # 3. Imprime el resultado utilizando print().
+
+print(f"La suma del set impar es  {total_set}")
 
 # 4. Ejecuta el programa.
 
