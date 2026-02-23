1ebfdc22bc7457e73d8e507631ad88448ce00734
Mayra Alzate <nadja.minni@gmail.com>
2026-02-23 15:41:06 -0500

    Ejerc 10 + README.

--- README.md
+++ README.md
@@ -16,5 +16,6 @@ La temática a emplear para la creación de los ejercicios esta basada en los te
 6. Ejercicio 6: Control de acceso a usuario con operadores lógicos y datos de tipo booleano.
 7. Ejercicio 7: Verificador de Edad.
 8. Ejercicio 08: Verificador de números pares-impares.
-9. Ejercicio 09: Cálculo de promedio de una lista con "len ()"
+9. Ejercicio 09: Cálculo de promedio de una lista con "len ()".
+10. Ejercicio 10: Crear una lista con la función range().
 
---
+++ ejercicios/ejercicio_10.py
@@ -0,0 +1,12 @@
+# Ejercicio 10: Crear una lista con la función range().
+
+# Descripción: Con la ayuda de la función range, generaremos números que posteriormente se convertiran en una lista.La función tiene un inicio, un fin y un salto que es definido en los argumentos de la misma. 
+
+# 1. Definimos la función range() con los 3 parámetros que configuran el inicio, el fin y el salto que queremos que se ejecute. Ej: inicio 2, final 20 con un salto de 3. Así : range(2,20,3).
+
+# 2.Para convertir el resultado de la función en una lista, imprimimos la lista conteniendo el rango.
+
+# 3. Ejecutamos el script.
+
+# Tip: Debemos referenciar el rango dentro de los argumentos de la función.
+
