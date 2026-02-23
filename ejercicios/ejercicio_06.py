6181e71b64042d1a33a0510f6404dd7460bc90f4
Mayra Alzate <nadja.minni@gmail.com>
2026-02-23 17:04:03 -0500

    puntos en 6

--- ejercicios/ejercicio_06.py
+++ ejercicios/ejercicio_06.py
@@ -10,7 +10,7 @@ contrasena_sistema =123456
 
 # 2. Estructure la captura de datos acorde a las variables definidas anteriormente. Recuerde que es imprescindible el uso del "input".
 
-usuario= input("Ingrese el usuario")
+usuario= input("Ingrese el usuario: ")
 contrasena= int (input("Ingrese la contraseña"))
 
 # 3. Defina la estructura de ciclos de acuerdo a las condiciones que permitan el correcto uso de las variables para acceder con éxito en el programa. 
