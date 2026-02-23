844ce70ecafefdd02d5ecfdda53d9e4c51881938
Mayra Alzate <nadja.minni@gmail.com>
2026-02-23 16:56:58 -0500

    correcion 6

--- ejercicios/ejercicio_06.py
+++ ejercicios/ejercicio_06.py
@@ -5,9 +5,20 @@
 
 # 1. Defina las variables que permitirán el acceso correcto del programa.
 
+usuario_sistema= "Emily"
+contrasena_sistema =123456
+
 # 2. Estructure la captura de datos acorde a las variables definidas anteriormente. Recuerde que es imprescindible el uso del "input".
 
+usuario= input("Ingrese el usuario")
+contrasena= int (input("Ingrese la contraseña"))
+
 # 3. Defina la estructura de ciclos de acuerdo a las condiciones que permitan el correcto uso de las variables para acceder con éxito en el programa. 
+if usuario_sistema == usuario and contrasena_sistema == contrasena:
+    print("Bienvenido al sistema")
+
+else:
+    print("Acceso denegado. Intente de nuevo.")
 
 # 4. Ejecute el programa.
 
